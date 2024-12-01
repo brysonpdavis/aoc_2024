f6f1c35bea5c1e8fdda14fbc11849bd2f3efdb7c
bryson <brysonpd@gmail.com>
2024-12-01 00:35:30 -0500

    format

--- 1/day1.exs
+++ 1/day1.exs
@@ -1,11 +1,12 @@
 # part 1
 
-{left, right} = File.read!("1/input.txt")
-|> String.split("\n")
-|> Enum.map(&String.split(&1, " ", trim: true))
-|> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
-|> Enum.map(&List.to_tuple/1)
-|> Enum.unzip()
+{left, right} =
+  File.read!("1/input.txt")
+  |> String.split("\n")
+  |> Enum.map(&String.split(&1, " ", trim: true))
+  |> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
+  |> Enum.map(&List.to_tuple/1)
+  |> Enum.unzip()
 
 left_sorted = Enum.sort(left)
 right_sorted = Enum.sort(right)
@@ -14,7 +15,6 @@ Enum.zip(left_sorted, right_sorted)
 |> Enum.map(fn {l, r} -> abs(l - r) end)
 |> Enum.sum()
 |> IO.puts()
-
 
 # part 2
 

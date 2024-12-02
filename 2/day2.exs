0c9a992bba3387cc5cdb79c4064480d85289df15
bryson <brysonpd@gmail.com>
2024-12-02 01:32:21 -0500

    more cleanup

--- 2/day2.exs
+++ 2/day2.exs
@@ -6,22 +6,24 @@ defmodule Day02 do
     |> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
   end
 
+  defp get_adjacent_pairs(report) do
+    Enum.zip(report, tl(report))
+  end
+
+  defp all_diffs_within_bounds(report) do
+    Enum.all?(get_adjacent_pairs(report), fn {x, y} -> 1 <= abs(x - y) and abs(x - y) <= 3 end)
+  end
+
+  defp all_descending(report) do
+    Enum.all?(get_adjacent_pairs(report), fn {x, y} -> x > y end)
+  end
+
+  defp all_ascending(report) do
+    Enum.all?(get_adjacent_pairs(report), fn {x, y} -> x < y end)
+  end
+
   defp is_safe?(report) do
-    get_adjacent_pairs = fn report -> Enum.zip(report, tl(report)) end
-
-    all_diffs_within_bounds = fn report ->
-      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> 1 <= abs(x - y) and abs(x - y) <= 3 end)
-    end
-
-    all_descending = fn report ->
-      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> x > y end)
-    end
-
-    all_ascending = fn report ->
-      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> x < y end)
-    end
-
-    all_diffs_within_bounds.(report) and (all_descending.(report) or all_ascending.(report))
+    all_diffs_within_bounds(report) and (all_descending(report) or all_ascending(report))
   end
 
   def part1 do
@@ -30,18 +32,18 @@ defmodule Day02 do
     |> IO.puts()
   end
 
+  defp get_all_permutations_of_length_minus_one(report) do
+    Enum.map(0..(length(report) - 1), fn i -> List.delete_at(report, i) end)
+  end
+
+  defp is_any_permutation_safe?(report) do
+    get_all_permutations_of_length_minus_one(report)
+    |> Enum.any?(&is_safe?/1)
+  end
+
   def part2 do
-    get_all_permutations_of_length_minus_one = fn report ->
-      Enum.map(0..(length(report) - 1), fn i -> List.delete_at(report, i) end)
-    end
-
-    is_any_permutation_safe? = fn report ->
-      get_all_permutations_of_length_minus_one.(report)
-      |> Enum.any?(&is_safe?/1)
-    end
-
     parse_reports()
-    |> Enum.count(is_any_permutation_safe?)
+    |> Enum.count(&is_any_permutation_safe?/1)
     |> IO.puts()
   end
 end

190b53302b8c64172065f6eb3f8577c3e401a819
bryson <brysonpd@gmail.com>
2024-12-02 01:19:44 -0500

    clean up

--- 2/day2.exs
+++ 2/day2.exs
@@ -1,26 +1,50 @@
+defmodule Day02 do
+  defp parse_reports do
+    File.read!("2/input.txt")
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
+  end
 
+  defp is_safe?(report) do
+    get_adjacent_pairs = fn report -> Enum.zip(report, tl(report)) end
 
-# parse
-levels = File.read!("2/input.txt")
-  |> String.split("\n")
-  |> Enum.map(&String.split(&1, " ", trim: true))
-  |> Enum.map(&Enum.map(&1, fn s -> String.to_integer(s) end))
+    all_diffs_within_bounds = fn report ->
+      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> 1 <= abs(x - y) and abs(x - y) <= 3 end)
+    end
 
-get_adjacent_pairs = fn list -> Enum.zip(list, tl(list)) end
+    all_descending = fn report ->
+      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> x > y end)
+    end
 
-# part 1
-all_diffs_within_bounds = fn list -> Enum.all?(get_adjacent_pairs.(list), fn {x, y} -> 1 <= abs(x - y) and abs(x - y) <= 3 end) end
-all_descending = fn list -> Enum.all?(get_adjacent_pairs.(list), fn {x, y} -> x > y end) end
-all_ascending = fn list -> Enum.all?(get_adjacent_pairs.(list), fn {x, y} -> x < y end) end
-is_safe? = fn list -> all_diffs_within_bounds.(list) and (all_descending.(list) or all_ascending.(list)) end
+    all_ascending = fn report ->
+      Enum.all?(get_adjacent_pairs.(report), fn {x, y} -> x < y end)
+    end
 
-num_safe_reports = Enum.count(levels, is_safe?)
+    all_diffs_within_bounds.(report) and (all_descending.(report) or all_ascending.(report))
+  end
 
-IO.inspect(num_safe_reports)
+  def part1 do
+    parse_reports()
+    |> Enum.count(&is_safe?/1)
+    |> IO.puts()
+  end
 
-# part 2
-get_all_permutations_of_length_minus_one = fn list -> Enum.map(0..(length(list) - 1), fn i -> List.delete_at(list, i) end) end
+  def part2 do
+    get_all_permutations_of_length_minus_one = fn report ->
+      Enum.map(0..(length(report) - 1), fn i -> List.delete_at(report, i) end)
+    end
 
-num_safe_reports = Enum.count(levels, fn list -> Enum.any?(get_all_permutations_of_length_minus_one.(list), is_safe?) end)
+    is_any_permutation_safe? = fn report ->
+      get_all_permutations_of_length_minus_one.(report)
+      |> Enum.any?(&is_safe?/1)
+    end
 
-IO.inspect(num_safe_reports)
+    parse_reports()
+    |> Enum.count(is_any_permutation_safe?)
+    |> IO.puts()
+  end
+end
+
+Day02.part1()
+Day02.part2()

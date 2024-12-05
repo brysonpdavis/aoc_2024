42ac85e8c1bcc8ec50057ab9b5db596a434f3d3e
bryson <brysonpd@gmail.com>
2024-12-05 02:43:18 -0500

    rework of day 3

--- 3/day3.exs
+++ 3/day3.exs
@@ -1,96 +1,130 @@
-defmodule Day03 do
+defmodule Day03Again do
   defp read do
     File.read!("3/input.txt")
-  end
-
-  def segment_start_string do
-    "mul("
-  end
-
-  defp segment_end_string do
-    ")"
-  end
-
-  defp segment_separator_str do
-    ","
-  end
-
-  def find_all_indexes_of_substring(string, substring, plus \\ 0) do
-    string
-    |> :binary.match(segment_start_string())
-    |> case do
-      :nomatch ->
-        []
-
-      {idx, _} ->
-        [
-          idx + plus
-          | find_all_indexes_of_substring(
-              String.slice(string, idx + 1, String.length(string) + idx + 1),
-              substring,
-              plus + idx + 1
-            )
-        ]
-    end
   end
 
   def part1 do
-    input_str = read()
+    read()
+    |> Parser.Part1.parse(:start)
+    |> IO.inspect()
+  end
 
-    find_all_indexes_of_substring(input_str, segment_start_string())
-    |> Enum.map(&(&1 + String.length(segment_start_string())))
-    |> Enum.map(fn start_index ->
-      res =
-        :binary.match(
-          String.slice(input_str, start_index, String.length(input_str)),
-          segment_end_string()
-        )
+  def part2 do
+    read()
+    |> Parser.Part2.parse(:start)
+    |> IO.inspect()
+  end
+end
 
-      case res do
+# states are named after what was most recently found: start, mul, comma
+defmodule Parser do
+  defmodule Part1 do
+    @segment_start_string "mul("
+
+    @segment_end_string ")"
+
+    @segment_separator_str ","
+
+    def parse(str, :start) do
+      case :binary.match(str, @segment_start_string) do
         :nomatch ->
-          nil
+          0
 
-        {end_index, _} ->
-          String.slice(input_str, start_index, end_index)
+        {match_idx, len} ->
+          str
+          |> String.slice((match_idx + len)..-1//1)
+          |> parse(:mul)
       end
-    end)
-    |> Enum.filter(&(&1 != nil))
-    |> Enum.map(&String.split(&1, segment_separator_str()))
-    # parse int pairs
-    |> Enum.map(fn
-      [x, y] ->
-        case {Integer.parse(x), Integer.parse(y)} do
-          {{x_int, ""}, {y_int, ""}} -> x_int * y_int
-          _ -> nil
-        end
+    end
 
-      _ ->
-        nil
-    end)
-    |> Enum.filter(&(&1 != nil))
-    |> Enum.sum()
-    |> IO.puts()
+    def parse(str, :mul) do
+      case Integer.parse(str) do
+        :error -> parse(str, :start)
+        {num, @segment_separator_str <> remainder} -> parse(remainder, :comma, num)
+        _ -> parse(str, :start)
+      end
+    end
+
+    def parse(str, :comma, num) do
+      case Integer.parse(str) do
+        :error -> parse(str, :start)
+        {num2, @segment_end_string <> remainder} -> num * num2 + parse(remainder, :start)
+        _ -> parse(str, :start)
+      end
+    end
+  end
+
+  defmodule Part2 do
+    @segment_start_string "mul("
+
+    @segment_end_string ")"
+
+    @segment_separator_str ","
+
+    @activate_str "do()"
+
+    @deactivate_str "don't()"
+
+    defp get_slice_after_match(str, {match_idx, match_len}) do
+      str
+      |> String.slice((match_idx + match_len)..-1//1)
+    end
+
+    def parse(str, :start) do
+      off_match_result = :binary.match(str, @deactivate_str)
+      start_match_result = :binary.match(str, @segment_start_string)
+
+      case {off_match_result, start_match_result} do
+        {_, :nomatch} ->
+          0
+
+        {:nomatch, match} ->
+          str
+          |> get_slice_after_match(match)
+          |> parse(:mul)
+
+        {{off_match_idx, off_match_len}, {start_match_idx, start_match_len}} ->
+          if off_match_idx < start_match_idx do
+            str
+            |> get_slice_after_match({off_match_idx, off_match_len})
+            |> parse(:off)
+          else
+            str
+            |> get_slice_after_match({start_match_idx, start_match_len})
+            |> parse(:mul)
+          end
+      end
+    end
+
+    def parse(str, :mul) do
+      case Integer.parse(str) do
+        :error -> parse(str, :start)
+        {num, @segment_separator_str <> remainder} -> parse(remainder, :comma, num)
+        _ -> parse(str, :start)
+      end
+    end
+
+    def parse(str, :comma, num) do
+      case Integer.parse(str) do
+        :error -> parse(str, :start)
+        {num2, @segment_end_string <> remainder} -> num * num2 + parse(remainder, :start)
+        _ -> parse(str, :start)
+      end
+    end
+
+    def parse(str, :off) do
+      case :binary.match(str, @activate_str) do
+        :nomatch ->
+          0
+
+        match ->
+          str
+          |> get_slice_after_match(match)
+          |> parse(:start)
+      end
+    end
   end
 end
 
-Day03.part1()
-
-# going to take more of a state machine approach
-defmodule Day03Try2 do
-  defp read do
-    File.read!("3/input.txt")
-  end
-
-  def segment_start_string do
-    "mul("
-  end
-
-  defp segment_end_string do
-    ")"
-  end
-
-  defp segment_separator_str do
-    ","
-  end
-
-end
+Day03Again.part1()
+Day03Again.part2()
